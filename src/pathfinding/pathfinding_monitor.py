b9f664f622cefd1d1951d60c6458c20a0449a700
Thuc Phan <phanhongthuc7749@gmail.com>
2025-04-11 04:59:56 +0700

    Enhance pathfinding_monitor to log function arguments for better monitoring of pathfinding processes

--- src/pathfinding/pathfinding_monitor.py
+++ src/pathfinding/pathfinding_monitor.py
@@ -3,6 +3,7 @@ Pathfinding Monitor
 
 This module provides a decorator to monitor the pathfinding process.
 """
+import inspect
 # TODO: Remove this import if not needed, otherwise, remove the pylint disable/enable comments.
 # pylint: disable=unused-import
 import tracemalloc # suggestion for tracing memory usage
@@ -20,7 +21,28 @@ def pathfinding_monitor(func: Pathfinder) -> Pathfinder:
     def wrapper(*args, **kwargs):
         # TODO: Set up actual monitoring logic here
         # TODO: Consider logging in a separate file for easier data collection
-        print("Starting pathfinding...")
+
+        # Get the function signature and bind arguments to it
+        sig = inspect.signature(func)
+        bound_args = sig.bind(*args, **kwargs)
+        bound_args.apply_defaults()  # Apply default values for missing arguments
+
+        # Print the function arguments for monitoring
+        print(f"Pathfinding: {func.__name__}, ", end="")
+        if "start_location" in bound_args.arguments:
+            start_location: MazeNode = bound_args.arguments["start_location"]
+            print(f"{start_location} to ", end="")
+        else:
+            print("??? to ", end="")
+        if "target_location" in bound_args.arguments:
+            target_location: MazeNode = bound_args.arguments["target_location"]
+            print(f"{target_location}")
+        elif "_target_location" in bound_args.arguments:
+            target_location: MazeNode = bound_args.arguments["_target_location"]
+            print(f"{target_location}")
+        else:
+            print("???")
+
         result: PathfindingResult = func(*args, **kwargs)
         # TODO: Add actual monitoring logic here
         def print_path(nodes: list[MazeNode]):
